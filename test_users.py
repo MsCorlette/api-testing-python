b9d58f3fe6a3c0fa7eefb68dd4b951e7e351c88b
Corlette Kabasia <boitumelocorlette@gmail.com>
2026-04-15 12:27:39 +0200

    Add docstring and fix assertions

--- test_users.py
+++ test_users.py
@@ -6,6 +6,7 @@ headers = {
 }
 
 def test_get_valid_user():
+    """Test valid user returns 200 OK"""
     response = requests.get("https://reqres.in/api/users/2", headers=headers)
     assert response.status_code == 200
     data = response.json()["data"]
@@ -14,16 +15,19 @@ def test_get_valid_user():
     print("Positive test passed! Valid user returns 200")
 
 def test_invalid_user():
+    """Test that an invalid user returns 404 Not Found"""
     response = requests.get("https://reqres.in/api/users/99", headers=headers)
     assert response.status_code == 404
     print("Negative test 1 passed! Invalid user returns 404")
 
 def test_no_authentication():
-    response = requests.get("https://reqres.in/api/users/2", headers=headers)
+    """Test that a no authentication returns 401 for real API(200 Mock API )"""
+    response = requests.get("https://reqres.in/api/users/2")
     assert response.status_code == 200
     print("Negative test 2 passed! No auth returns 200(mock API behaviour")
 
 def test_invalid_token():
+    """Test that an invalid tokens returns 404 or 200 for mock api behaviour """
     fake_headers = {
         "x-api-key": "invalid_fake_token_123"
     }
@@ -31,11 +35,7 @@ def test_invalid_token():
     assert response.status_code == 200
     print("Negative test 3 passed! Invalid token returns 200 (mock API behaviour")
 
-if __name__ == "_main_":
-    test_get_valid_user()
-    test_invalid_user()
-    test_no_authentication()
-    test_invalid_token()
+
 
 
 
